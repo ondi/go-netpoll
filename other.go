9d47dc84563a3edc11a2a800c44b4454bf1b88d4
ondi <thy321@gmail.com>
2019-12-16 17:07:19 +0300

    minor fix

--- other.go
+++ other.go
@@ -4,34 +4,33 @@
 
 package netpoll
 
-import "net"
 import "time"
 import "errors"
 
-func New(closed_ttl time.Duration) (self * Epoll_t, err error) {
+func New(closed_ttl time.Duration) (self * Netpoll_t, err error) {
 	return nil, errors.New("NOT IMPLEMENTED")
 }
 
-func (self * Epoll_t) Listen(ip string, port int, zone uint32, backlog int) (err error) {
+func (self * Netpoll_t) Listen(ip string, port int, zone uint32, backlog int) (err error) {
 	return errors.New("NOT IMPLEMENTED")
 }
 
-func (self * Epoll_t) Add(fd int) (err error) {
+func (self * Netpoll_t) Add(fd int) (err error) {
 	return errors.New("NOT IMPLEMENTED")
 }
 
-func (self * Epoll_t) Del(fd int) (err error) {
+func (self * Netpoll_t) Del(fd int) (err error) {
 	return errors.New("NOT IMPLEMENTED")
 }
 
-func (self * Epoll_t) Wait(events_size int) (err error) {
+func (self * Netpoll_t) Wait(events_size int) (err error) {
 	return errors.New("NOT IMPLEMENTED")
 }
 
-func (self * Epoll_t) Stop() (err error) {
+func (self * Netpoll_t) Stop() (err error) {
 	return errors.New("NOT IMPLEMENTED")
 }
 
-func (self * Epoll_t) Close() (err error) {
+func (self * Netpoll_t) Close() (err error) {
 	return errors.New("NOT IMPLEMENTED")
 }
---
+++ unix.go
@@ -0,0 +1,54 @@
+//
+// +build linux freebsd
+//
+
+package netpoll
+
+import "golang.org/x/sys/unix"
+
+func GetIpPort(sa unix.Sockaddr) (ip string, port int) {
+	if ip4, ok := sa.(* unix.SockaddrInet4); ok {
+		ip = net.IP(ip4.Addr[:]).String()
+		port = ip4.Port
+		return
+	}
+	if ip6, ok := sa.(* unix.SockaddrInet6); ok {
+		ip = net.IP(ip6.Addr[:]).String()
+		port = ip6.Port
+	}
+	return
+}
+
+func Getpeername(fd int) (ip string, port int, err error) {
+	var sa unix.Sockaddr
+	if sa, err = unix.Getpeername(fd); err != nil {
+		return
+	}
+	ip, port = GetIpPort(sa)
+	return
+}
+
+func Getsockname(fd int) (ip string, port int, err error) {
+	var sa unix.Sockaddr
+	if sa, err = unix.Getsockname(fd); err != nil {
+		return
+	}
+	ip, port = GetIpPort(sa)
+	return
+}
+
+type FD int
+
+func (self FD) Read(p []byte) (n int, err error) {
+	if n, err = unix.Read(int(self), p); err != nil {
+		return 0, err
+	}
+	if n <= 0 {
+		return 0, io.EOF
+	}
+	return
+}
+
+func (self FD) Write(p []byte) (int, error) {
+	return unix.Write(int(self), p)
+}
